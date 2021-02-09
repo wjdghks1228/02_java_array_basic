255de7eab1696033a33f56c974996bd596a0214e
19시_웹개발_KJH <19__KJH@DESKTOP-7SPL8CJ>
2021-02-09 19:50:58 +0900

    array basic

--- 02_java_array_basic/src/step2_01/array/ArrayEx01.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx01.java
@@ -1,9 +1,41 @@
 package step2_01.array;
 
+import java.util.Arrays;
+
 public class ArrayEx01 {
 
 	public static void main(String[] args) {
+		//1.일반 변수 ( 값 저장 )
+		int nVar = 0 ;
 		
+		//2. 주소 변수 ( 주소 저장 ) 
+		int [ ] arr = null;
+		arr = new int [ 5 ];
+		
+		System.out.println(arr);
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		
+		arr[ 0 ] = 10; 
+		arr[ 1 ] = 20; 
+		arr[ 2 ] = 30; 
+		arr[ 3 ] = 40; 
+		arr[ 4 ] = 50;
+		
+		System.out.println("배열 출력 방법 1");
+		for( int i = 0 ; i < 5 ; i++) {
+			System.out.println(arr[ i ]);
+		}
+		
+		System.out.println("배열 출력 방법 2");
+		System.out.println(Arrays.toString(arr));
+		
+		//System.out.println(arr[5]);
+		//java.lang.ArrayIndexOutOfBoundsException: 5
+		//배열 범위 초과 에러
 	}
 
 }
